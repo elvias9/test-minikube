06e82ba9f8e013ea69b7d6842c7e90e72fa39038
elvias9 <elvia.sicuro@gmail.com>
2025-06-29 14:59:20 +0200

    modified dockerfiles

--- containers/hello_world_server/index.js
+++ containers/hello_world_server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const message = require('./hello_world.json')
+//const message = require('./hello_world.json')
 const PORT = 4000;
 
 app.use(express.json());
 
 app.get('/', (req ,res) => {
-   res.status(200).json(message)
+   res.status(200).json({ "id": "1", "message": "Hello world"})
 })
 
 
